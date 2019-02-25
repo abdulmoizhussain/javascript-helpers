dbafbd05b6a742573504f612dbdeb1dfcfb8559b
Abdul Moiz Hussain <33151673+abdulmoizhussain@users.noreply.github.com>
2019-02-25 10:49:30 +0500

    Check for nested objects

---
+++ checkNested.js
@@ -0,0 +1,13 @@
+// https://stackoverflow.com/questions/2631001/test-for-existence-of-nested-javascript-object-key
+function checkNested(obj = {}, props = "") {
+  const propsArray = props.split('.');
+  for (let i = 0; i < propsArray.length; i++) {
+    if (!obj || typeof obj !== "object" || !(propsArray[i] in obj)) return false;
+    obj = obj[propsArray[i]];
+  }
+  return true;
+}
+module.exports = checkNested;
+
+// usage:
+// checkNested({ a: { b: { c: "" } } }, "a.b.c");
