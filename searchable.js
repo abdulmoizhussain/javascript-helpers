4ec6f3c8a9abb6aecc6435703a1de787702b17d8
Abdul Moiz Hussain <33151673+abdulmoizhussain@users.noreply.github.com>
2019-03-01 12:43:23 +0500

    Makes a string easily google searchable

--- searchable.js
+++ searchable.js
@@ -1,4 +1,4 @@
 function searchable(text) {
-//     return text.replace(/[@!#$%^&*(),?":{}|<> =/+;:'-\\_~`]/g, " ").replace(/  /g," ");
+//     return text.replace(/[@!#$%^&*(),?":{}|<> =/+;:'-\\_~`]+/g, " ").trim();
     return text.replace(/[^a-zA-Z0-9]+/g, " ").trim();
 }
