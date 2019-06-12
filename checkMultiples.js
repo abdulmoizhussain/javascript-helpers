24c9ccccdd56e8b99591b7afa860388d6e7f648a
Abdul Moiz Hussain <33151673+abdulmoizhussain@users.noreply.github.com>
2019-06-12 16:14:54 +0500

    Create checkMultiples.js

---
+++ checkMultiples.js
@@ -0,0 +1,26 @@
+function checkMultiples(source='', delim='') {
+	const lines = source.split(delim);
+	for (var i=0; i< lines.length; i++) {
+		for(var j=0; j< lines.length; j++) {
+			if (lines[i] === lines[j]) {
+				if (i === j) continue;
+				return "'" + lines[i] + "' found second time !";
+			}
+		}
+	}
+	return "";
+}
+/*
+Example usage:
+> checkMultiples(`Susan Hayes
+Sydney Williams
+Terry Herrera
+Theodore Wells
+Toni Smith
+Tracy Ellis
+Valerie Garza
+William Bean
+William Bean
+Veronica Cortez`, '\n');
+> "'William Bean' found second time !"
+*/
