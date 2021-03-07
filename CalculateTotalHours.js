5423ab74c285f75a642e35b07c20fed0f5356ad0
Abdul Moiz Hussain <33151673+abdulmoizhussain@users.noreply.github.com>
2021-03-07 23:45:29 +0500

    Update CalculateTotalHours.js

--- CalculateTotalHours.js
+++ CalculateTotalHours.js
@@ -1,8 +1,14 @@
 // calculate total hours;
 times = [];
 function calc(hours, minutes) {
-  const tem = new Date(0);
-  tem.setTime((hours*60*60*1000)+(minutes*60*1000));
-  times.push(tem.getTime());
-  return "Total Hours: " + times.reduce((accumulator, currentVal) => accumulator + currentVal, 0) / (1000*60*60);
+    const temp = new Date(0);
+    temp.setTime((hours * 60 * 60 * 1000) + (minutes * 60 * 1000));
+    times.push(temp.getTime());
+    
+    const totalHours = times.reduce((accumulator, currentVal) => accumulator + currentVal, 0) / (1000 * 60 * 60);
+
+    hours = Math.floor(totalHours);
+    minutes = Math.floor((totalHours % 1) * 60);
+
+    return `Total time: ${hours} hours ${minutes} minutes.`;
 }
